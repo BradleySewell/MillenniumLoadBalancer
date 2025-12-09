171c5320999669ceb15246ae7eb003ca022104c5
Bradley Sewell <bradleygsewell@hotmail.com>
2025-12-09 02:43:48 +0000

    updated startup console intro

--- MillenniumLoadBalancer.App/Infrastructure/LoadBalancerManager.cs
+++ MillenniumLoadBalancer.App/Infrastructure/LoadBalancerManager.cs
@@ -30,9 +30,10 @@ public class LoadBalancerManager : ILoadBalancerManager
 
     public void Initialize()
     {
-        _hostLogger.LogInformation("+-------------------------------------------------------------+");
-        _hostLogger.LogInformation("|                 MILLENNIUM LOAD BALANCER                    |");
-        _hostLogger.LogInformation("+-------------------------------------------------------------+");
+        Console.WriteLine("+-------------------------------------------------------------+");
+        Console.WriteLine("|                 MILLENNIUM LOAD BALANCER                    |");
+        Console.WriteLine("|                             V1                              |");
+        Console.WriteLine("+-------------------------------------------------------------+");
 
         var loadBalancerOptions = _configuration.GetSection("LoadBalancer").Get<LoadBalancerOptions>()
             ?? throw new InvalidOperationException("LoadBalancer configuration is missing");
